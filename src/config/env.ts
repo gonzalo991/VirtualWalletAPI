77bdba0f396eaa9b6f32bff4395125795fad35b1
Gonzalo Araya <gonxaraya991@gmail.com>
2026-04-17 19:08:13 -0300

    updated user routes for legibility and started jwt implementation

---
+++ src/config/env.ts
@@ -0,0 +1,5 @@
+// config/env.ts
+export const env = {
+  JWT_SECRET: process.env.JWT_SECRET!,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN!,
+};
--- src/modules/index.routes.ts
+++ src/modules/index.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import userRouter from "./user/index.js";
+import userRouter from "./user/user.routes.js";
 
 const router: Router = Router();
 
